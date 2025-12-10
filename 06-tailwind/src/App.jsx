c4c7fd4a3dd4ef75fb26ec80721f4c7b081efed0
Vishal kushwaha <v.kushwaha12ka3@gmail.com>
2025-12-10 14:16:10 +0530

    Understanding Installing tailwind done

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-full bg-blue-500">
+      <p className="text-5xl flex justify-center items-center min-h-screen font-bold underline">
+        Installing tailwind
+      </p>
+    </div>
+  );
+}
+
+export default App;
