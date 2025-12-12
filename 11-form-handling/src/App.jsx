a09940cde1958e71f8bab5a846f0438ba954d33e
Vishal kushwaha <v.kushwaha12ka3@gmail.com>
2025-12-12 18:43:59 +0530

    Form handling done

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+
+function App() {
+  return (
+    <div className="h-screen text-white bg-black flex justify-center items-center">
+      <div className="h-[90vh] w-150 rounded-4xl text-white border-2 border-gray-400 ">
+        <Header />
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 11-form-handling/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="flex font-bold justify-center py-10 p-10">
+          <h3 className="text-3xl tracking-tight">
+            This is The Form Created By Me
+          </h3>
+        </div>
+    )
+}
+
+export default Header
